8919fd2b6fdad8b7bf96b202854264d7064665d4
akhaled247 <abdullahkhaled0224@gmail.com>
2026-07-30 10:24:10 -0400

    Added all_surface to LTL formula instead of forced sequential

--- tests/test_ma_eval_formula.py
+++ tests/test_ma_eval_formula.py
@@ -4,5 +4,5 @@ from utils.deploy_meta import MA_EVAL_FORMULA_DEFAULT
 
 def test_ma_eval_formula_team_entrapped_gate():
     assert MA_EVAL_FORMULA_DEFAULT == (
-        "((!surface_0 & !surface_1) U all_entrapped) & (F surface_0 & F surface_1)"
+        "((!surface_0 & !surface_1) U all_entrapped) & F all_surface"
     )
