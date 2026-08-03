Attempted to bugfix SA to MA eval diff; zeroes on gremlins and adjust GenZ formula

src/deploy/ma_rollout_ppo.py
@@ -18,7 +18,7 @@ from envs.sar_deploy import (
 )
 from envs.seq_wrapper import sar_task
 from sequence.search import NoPathsException
-from utils.deploy_meta import MA_EVAL_ENV_DEFAULT, MA_EVAL_FORMULA_DEFAULT
+from utils.deploy_meta import MA_EVAL_ENV_DEFAULT, MA_EVAL_FORMULA_DEFAULT, warn_if_fragile_ma_formula
 
 TRAIN_ENV = "PointLTL0MASAR1WC-v0"
 EVAL_ENV = MA_EVAL_ENV_DEFAULT
@@ -41,6 +41,8 @@ def simulate_ma_ppo(
     random.seed(seed)
     np.random.seed(seed)
     torch.random.manual_seed(seed)
+
+    warn_if_fragile_ma_formula(formula)
 
     env, model, search, props, algo = build_sar_ltl_eval_stack(
         train_env,

src/envs/ldba_wrapper.py
@@ -49,6 +49,9 @@ class LDBAWrapper(gymnasium.Wrapper):
                 # 16 dim for agent status, 16 dim for reach, and 16 dim for avoid
                 'features': spaces.Box(-np.inf, np.inf, (48,), dtype=np.float32)
             })
+            self.agent_obs_keys = [
+                "accelerometer", "velocimeter", "gyro", "magnetometer", "wall_sensor",
+            ]
         elif "SAR" in env.spec.id:
             task = sar_task(env)
             num_agents = getattr(task, "agent_num", 1)
@@ -76,7 +79,13 @@ class LDBAWrapper(gymnasium.Wrapper):
             self.observation_space = spaces.Dict({
                 'features': spaces.Box(0, 1, (obs_dim, obs_dim, 1), dtype=np.float32)
             })
-        self.agent_obs_keys = ["accelerometer", "velocimeter", "gyro", "magnetometer", "wall_sensor"]
+            self.agent_obs_keys = [
+                "accelerometer", "velocimeter", "gyro", "magnetometer", "wall_sensor",
+            ]
+        else:
+            self.agent_obs_keys = [
+                "accelerometer", "velocimeter", "gyro", "magnetometer", "wall_sensor",
+            ]
         self.region_order = env.get_propositions()
         self.terminate_on_acceptance = False
         self.ldba = None